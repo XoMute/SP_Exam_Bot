91d52aabeaa2333202a187d4056d25d6675357f1
Ddone <mynameisthenekit@gmail.com>
2019-06-21 20:45:56 +0300

    Stuff (if you want to host you need to change file path in code)

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
 import telebot;
+from telebot import types
+from subprocess import call
 bot = telebot.TeleBot('838535643:AAFqEVm3RXjszzemWqIBF6bPCfy4Pqjj0Hw')
+
+kb = types.ReplyKeyboardMarkup()
+kb.add('USE16','USE32')
+
+usecache = {}
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+    if message.text.startswith('USE'):
+        usecache[message.chat.id] = message.text
+        print(usecache)
+        bot.send_message(message.chat.id,'Send me your asm', reply_markup=kb)
+        return
+
+    if message.text == '/start' or not usecache.get(message.chat.id):
+        bot.send_message(message.chat.id,'Hi', reply_markup=kb)
+        return;
+
+    process_asm(message.chat.id,message.text,usecache[message.chat.id])
+
+    
+
+def process_asm(id,code,use):
+
+    print('processing asm')
+    print(code)
+    with open('/home/ddone/Code/masm/BOT.ASM','w') as o:
+        o.write(''' .386
+DATA SEGMENT USE32
+a db 1
+b dw 2
+c dd 3
+w dq 4
+DATA ENDS
+CODE SEGMENT {}
+assume cs:code, ds:data
+{}
+code ends
+end
+                '''.format(use,code))
+    call(''' dosbox -c "TASM.EXE /l BOT.ASM" -c "exit" ''',shell=True)
+
+    with open('/home/ddone/Code/masm/BOT.LST') as i:
+        bot.send_message(id,'```{}```'.format(i.read()))
+
+
+
+
 
 bot.polling(none_stop = True, interval = 0)
